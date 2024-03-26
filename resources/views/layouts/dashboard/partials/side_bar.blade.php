1474fdd68870d27ea04cb28b31abcc2ebd9d8d22
abdoMagdy <bodomagdy@yahoo.com>
2024-03-27 00:39:27 +0200

    setup breeze for auth

--- resources/views/layouts/dashboard/partials/side_bar.blade.php
+++ resources/views/layouts/dashboard/partials/side_bar.blade.php
@@ -1,22 +1,24 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ route('dashboard') }}" class="brand-link">
         <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Multi Store</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+        @auth
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                </div>
+                <div class="info">
+                    <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                </div>
             </div>
-            <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
-            </div>
-        </div>
+        @endauth
 
         <!-- SidebarSearch Form -->
         <div class="form-inline">
@@ -61,13 +63,13 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-th"></i>
-                        <p>
-                            Simple Link
-                            <span class="right badge badge-danger">New</span>
-                        </p>
-                    </a>
+                    <form action="{{ route('logout') }}" method="POST" class="nav-link">
+                        @csrf
+                        <button type="submit" class="text-danger border-0 bg-transparent w-100 text-left">
+                            <i class="fa fa-arrow-circle-right text-danger" aria-hidden="true"></i>
+                            logOut
+                        </button>
+                    </form>
                 </li>
             </ul>
         </nav>
